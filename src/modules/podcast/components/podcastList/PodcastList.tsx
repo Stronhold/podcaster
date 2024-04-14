3ba2951271b9684d86cbbc91efa9987c5dd455b1
stronholds <sergioausin1993@gmail.com>
2024-04-14 11:01:42 +0200

    test: add test for main page

--- src/modules/podcast/components/podcastList/PodcastList.tsx
+++ src/modules/podcast/components/podcastList/PodcastList.tsx
@@ -3,15 +3,18 @@ import { Podcast } from '../../models/Podcast';
 import { PodcastCard } from './podcastCard/PodcastCard';
 import { styles } from './styles';
 import { EmptyPodcastCard } from './podcastCard/EmptyPodcastCard';
+import { DataTestid } from '../../../../shared/utils/dataTestId';
 
 const CARDS_PER_LINE = 4;
 interface Props {
   podcasts: Array<Podcast>;
 }
 export const PodcastList = ({ podcasts }: Props) => {
-  const extraEmptyCards = CARDS_PER_LINE - (podcasts.length % 4);
+  if (!podcasts || podcasts.length === 0) return null;
+  const extraEmptyCards =
+    podcasts.length % 4 === 0 ? 0 : CARDS_PER_LINE - (podcasts.length % 4);
   return (
-    <Box sx={styles.container}>
+    <Box sx={styles.container} data-testid={DataTestid.PodcastContainer}>
       {podcasts.map((p) => (
         <PodcastCard podcast={p} key={p.id} />
       ))}
